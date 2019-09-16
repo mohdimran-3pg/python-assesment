bed4e7b074ac03866aba1318a00ffc203a9c51f0
mohdimran-3pg <mohd.imran.3pg@gmail.com>
2019-09-16 13:14:49 +0530

    Code Refactoring

--- predefined_alphabet_guess.py
+++ predefined_alphabet_guess.py
@@ -6,27 +6,31 @@ class LargeStringException(Exception):
    pass
 class NotSameStringException(Exception):
    """Raised when the input string is not same to predefined string"""
-   pass   
+   pass
+
+def startPredefinedAlphabetGuessProcess(alphabet):
+  while True:
+     try:
+         input_string = (input("Enter a alphabet: ")).lower()
+         if len(input_string) < len(alphabet):
+             raise SmallStringException
+         elif len(input_string) > len(alphabet):
+             raise LargeStringException
+         elif input_string != alphabet:
+             raise NotSameStringException 
+
+         break
+     except SmallStringException:
+         print("This alphabet is too small, try again!")
+         print()
+     except LargeStringException:
+         print("This alphabet is too large, try again!")
+         print()
+     except NotSameStringException:
+         print("This alphabet is not to predefined alphabet , try again!")
+         print()    
+  print("Congratulations! You guessed it correctly.")      
+
 
 answerString = "imran"
-while True:
-   try:
-       input_string = (input("Enter a string: ")).lower()
-       if len(input_string) < len(answerString):
-           raise SmallStringException
-       elif len(input_string) > len(answerString):
-           raise LargeStringException
-       elif input_string != answerString:
-           raise NotSameStringException 
-
-       break
-   except SmallStringException:
-       print("This string is too small, try again!")
-       print()
-   except LargeStringException:
-       print("This string is too large, try again!")
-       print()
-   except NotSameStringException:
-       print("This string is not to predefined string , try again!")
-       print()    
-print("Congratulations! You guessed it correctly.")
+startPredefinedAlphabetGuessProcess(answerString)
